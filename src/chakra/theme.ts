c0bfa58d151f83bd7c70dc78cd23d9700cee0a48
hristo2612 <hristoapps@gmail.com>
2023-02-12 01:51:09 +0200

    Basic Navbar

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,12 +1,13 @@
 import '@fontsource/open-sans/300.css';
 import '@fontsource/open-sans/400.css';
 import '@fontsource/open-sans/700.css';
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react';
+import { Button } from './button';
 
 export const theme = extendTheme({
   colors: {
     brand: {
-      100: "#FF3c00",
+      100: '#FF3c00',
     },
   },
   fonts: {
@@ -15,8 +16,11 @@ export const theme = extendTheme({
   styles: {
     global: () => ({
       body: {
-        bg: "gray.200"
-      }
-    })
-  }
+        bg: 'gray.200',
+      },
+    }),
+  },
+  components: {
+    Button,
+  },
 });
