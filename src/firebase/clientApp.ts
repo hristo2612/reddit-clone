f04afb715f45498e1878ae9e9c3559cff19e253b
bogoehri <hristo_bogoev@starsgroup.com>
2023-02-13 20:49:58 +0200

    Enable Google Analytics

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -2,6 +2,7 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -18,5 +19,6 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const firestore = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
 
-export { app, auth, firestore, storage };
+export { app, auth, firestore, storage, analytics };
